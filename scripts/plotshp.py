60e984c8f5d5e23ebbaa3be57f7e58753000c10c
Fritz Reese <fritzoreese@gmail.com>
2018-10-04 15:01:40 -0400

    Allow plotting multiple files at a time

--- scripts/plotshp.py
+++ scripts/plotshp.py
@@ -16,29 +16,44 @@ def plot_polygons(reader, *pltargs, **pltkws):
         y = [pt[1] for pt in polygon.points]
         pyplot.plot(x, y, *pltargs, **pltkws)
 
+def plot_reader(reader):
+    if reader.shapeType in (shapefile.POINT, shapefile.POINTZ):
+        plot_points(reader, marker='o', markersize=4)
+    elif reader.shapeType in (shapefile.POLYGON, shapefile.POLYGONZ):
+        plot_polygons(reader)
+    else:
+        raise RuntimeError('unsupported shape type '+str(reader.shapeType))
+
 def main(*args):
     p = argparse.ArgumentParser(description='Plot a shapefile.')
-    p.add_argument('shapefile', metavar='PATH', type=str,
-            help='Basename to shapefile. Expects PATH.{shp,shx,dbf} to exist.')
+    p.add_argument('shapefiles', metavar='PATH...', type=str, nargs='+',
+            help='Basenames to shapefiles.'
+                 ' *.{shp,shx,dbf} should exist for each PATH.')
 
-    path = p.parse_args(args).shapefile
-    reader = shapefile.Reader(path)
-    try:
-        pyplot.figure()
-        if reader.shapeType in (shapefile.POINT, shapefile.POINTZ):
-            plot_points(reader, marker='o', markersize=4)
-        elif reader.shapeType in (shapefile.POLYGON, shapefile.POLYGONZ):
-            plot_polygons(reader)
-        else:
-            raise RuntimeError('unsupported shape type '+str(reader.shapeType))
-        pyplot.show()
-    except:
-        raise
-    finally:
-        if int(shapefile.__version__.split('.')[0]) > 1:
-            reader.close()
+    paths = p.parse_args(args).shapefiles
+    excs = list()
+    pyplot.figure()
 
-    return 0
+    for path in paths:
+        try:
+            reader = shapefile.Reader(path)
+            plot_reader(reader)
+        except Exception as e:
+            excs.append(e)
+        finally:
+            if int(shapefile.__version__.split('.')[0]) > 1:
+                reader.close()
+
+    ret = 0
+    if excs:
+        ret = 1
+        sys.stderr.write('Ignored the following errors:\n')
+        for exc in excs:
+            sys.stderr.write('  %s: %s\n' % (type(exc).__name__, str(exc)))
+
+    pyplot.show()
+
+    return ret
 
 if __name__ == '__main__':
     sys.exit(main(*sys.argv[1:]))
